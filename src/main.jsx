04b145fe25f843cc8222a4bd294dbfad472c2ab3
amirs-web <siami.amir4@gmail.com>
2024-05-14 15:35:14 +0330

    Update useUpdateUser

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 
